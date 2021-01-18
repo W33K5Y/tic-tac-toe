652d802cee4a422de0ceac6de75ca0e9310e23e8
Sam Weeks <weeksy_17@icloud.com>
2021-01-18 07:46:47 +0000

    switch s added

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-
+// ! Declare variables
 const section1 = document.querySelector('.section-one');
 const mainDiv = document.getElementById('main-wrap');
 let whosGo = 0;
+// ! Cross svg innerHTML
 const cross = `<svg
 class="cross"
 xmlns="http://www.w3.org/2000/svg"
@@ -13,6 +14,7 @@ viewBox="0 0 24 24"
   d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"
 />
 </svg>`;
+// ! Circle svg innerHTML
 const circle = `      <svg
 class="circle"
 xmlns="http://www.w3.org/2000/svg"
@@ -24,20 +26,90 @@ viewBox="0 0 24 24"
   d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12z"
 />
 </svg>`;
+let keepTrackPlayerOne = [];
+let keepTrackPlayerTwo = [];
+let winnerMessage = "";
+// ! start to loop through #main-wrap children and attach a listener to each one
 
 for (prop of mainDiv.children) {
     prop.addEventListener('click',addSVGs);
 }
 
 // ! this adds the circles and croses in player order and then removes the listener on the specific el 
+
 function addSVGs(e) {
     whosGo ++;
     if(whosGo % 2 === 0) {
       this.innerHTML = circle;
       this.removeEventListener("click",addSVGs);
+      keepTrackPlayerTwo += this.id; 
      } else{
       this.innerHTML = cross;
       this.removeEventListener("click",addSVGs);
+      keepTrackPlayerOne += this.id; 
     } 
+    switch(keepTrackPlayerOne) {
+ //  Up Top
+      case '012': console.log('testy'); break;
+      case '021': console.log('testy'); break;
+      case '120': console.log('testy'); break;
+      case '102': console.log('testy'); break;
+      case '201': console.log('testy'); break;
+      case '210': console.log('testy'); break;
+// Middle 
+      case '345': console.log('testy'); break;
+      case '354': console.log('testy'); break;
+      case '543': console.log('testy'); break;
+      case '534': console.log('testy'); break;
+      case '453': console.log('testy'); break;
+      case '435': console.log('testy'); break;
+// bottom
+      case '678': console.log('testy'); break;
+      case '687': console.log('testy'); break;
+      case '786': console.log('testy'); break;
+      case '768': console.log('testy'); break;
+      case '876': console.log('testy'); break;
+      case '867': console.log('testy'); break; 
+// left
+      case '036': console.log('testy'); break;
+      case '063': console.log('testy'); break;
+      case '360': console.log('testy'); break;
+      case '306': console.log('testy'); break;
+      case '630': console.log('testy'); break;
+      case '603': console.log('testy'); break;
+// middle
+      case '147': console.log('testy'); break;
+      case '174': console.log('testy'); break;
+      case '417': console.log('testy'); break;
+      case '471': console.log('testy'); break;
+      case '741': console.log('testy'); break;
+      case '714': console.log('testy'); break;
+// right 
+      case '258': console.log('testy'); break;
+      case '285': console.log('testy'); break;
+      case '582': console.log('testy'); break;
+      case '528': console.log('testy'); break;
+      case '852': console.log('testy'); break;
+      case '825': console.log('testy'); break;
+// diagonal left
+      case '048': console.log('testy'); break;
+      case '084': console.log('testy'); break;
+      case '408': console.log('testy'); break;
+      case '480': console.log('testy'); break;
+      case '840': console.log('testy'); break;
+      case '804': console.log('testy'); break;
+// diagonal right
+      case '246': console.log('testy'); break;
+      case '264': console.log('testy'); break;
+      case '462': console.log('testy'); break;
+      case '426': console.log('testy'); break;
+      case '642': console.log('testy'); break;
+      case '624': console.log('testy'); break;
 
+    }
 }
+
+
+// ! function to keep track of who wins the game 
+
+
