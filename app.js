fcaa0841c3a57045ea59abaf9f1240f96f5df51c
Sam Weeks <weeksy_17@icloud.com>
2021-01-18 21:15:35 +0000

    made mobile friendly

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 // ! Declare variables
-const section1 = document.querySelector('.section-one');
-const mainDiv = document.getElementById('main-wrap');
+const section1    = document.querySelector('.section-one');
+const mainDiv     = document.getElementById('main-wrap');
+const startDiv    = document.getElementById('start-game');
+const startButton = document.getElementById('start-button');
+const endDiv      = document.getElementById('end-game');
+const endButton = document.getElementById('end-button');
+const endH1 = document.getElementById('end-h1');
+
+const headerDiv   = document.querySelector('.header-display-none');
 let whosGo = 0;
 // ! Cross svg innerHTML
 const cross = `<svg
@@ -26,14 +33,24 @@ viewBox="0 0 24 24"
   d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12z"
 />
 </svg>`;
-let winnerMessage = "";
+// ! create a function to get rid of first Div 
+startButton.addEventListener("click",startGame);
+function startGame(e) {
+if(this){
+  startDiv.style.display = "none";
+  headerDiv.className="header-display-show";
+}
+}
+// ! create a function to declare winnerDiv
+
+
 // ! start to loop through #main-wrap children and attach a listener to each one
 
 for (prop of mainDiv.children) {
   prop.addEventListener('click',addSVGs);
 }
 
-// ! this adds the circles and croses in player order and then removes the listener on the specific el 
+// ! this adds the circles and croses in player order and then removes the listener on the specific el ==>
 
 let keepTrackPlayerOne = [];
 let keepTrackPlayerTwo = [];
@@ -46,92 +63,54 @@ function addSVGs(e) {
       keepTrackPlayerTwo.push(this.id); 
       keepTrackPlayerTwo.sort();
       let doesPlayerTwoWin = keepTrackPlayerTwo.toString();
-      whosTheWinner(doesPlayerTwoWin,'Two')
+      whosTheWinner(doesPlayerTwoWin,'Player Two Wins')
 
      } else{
       this.innerHTML = cross;
       this.removeEventListener("click",addSVGs);
       keepTrackPlayerOne.push(this.id); 
       keepTrackPlayerOne.sort();
-     let doesPlayerOneWin = keepTrackPlayerOne.toString();
-    console.log(doesPlayerOneWin)
-      whosTheWinner(doesPlayerOneWin,"One")
+      let doesPlayerOneWin = keepTrackPlayerOne.toString();
+      whosTheWinner(doesPlayerOneWin,"Player One Wins");
     } 
 
 }
-function whosTheWinner(player, message) {
-  switch(player) {
-    case "0,1,2" : console.log(`Player ${message} Wins`); break;
-    case '3,4,5' : console.log(`Player ${message} Wins`); break;
-    case "6,7,8" : console.log(`Player ${message} Wins`); break;
-    case "0,3,6" : console.log(`Player ${message} Wins`); break;
-    case "1,4,7" : console.log(`Player ${message} Wins`); break;
-    case "2,5,8" : console.log(`Player ${message} Wins`); break;
-    case "0,4,8" : console.log(`Player ${message} Wins`); break;
-    case "2,4,6" : console.log(`Player ${message} Wins`); break;
-  
-  }
+
+// ! function to fit in above listener this declares the winning result ==>
+
+function whosTheWinner(player, printMessage) {
+    player.includes("0,1,2") ? winnerDiv(printMessage) :
+    player.includes("3,4,5") ? winnerDiv(printMessage) :
+    player.includes("6,7,8") ? winnerDiv(printMessage) :
+    player.includes("0,3,6") ? winnerDiv(printMessage) :
+    player.includes("1,4,7") ? winnerDiv(printMessage) :
+    player.includes("2,5,8") ? winnerDiv(printMessage) :
+    player.includes("0,4,8") ? winnerDiv(printMessage) :
+    player.includes("2,4,6") ? winnerDiv(printMessage) :
+    whosGo === 9         ? winnerDiv("This Is A Draw") : null; 
 }
 
+function winnerDiv(message) {
+  headerDiv.className="header-display-none";
+  endDiv.className="end end-display-show";
+  endH1.innerHTML = `${message}`;
+}
 
-// ! function to keep track of who wins the game 
+// ! <== create a function and listener for the end-button to restart game
+// ! clear existing conditions and restart game all within the listener ====>
 
-switch(keepTrackPlayerOne) {
-  //  Up Top
-       case '012': console.log('testy'); break;
-       case '021': console.log('testy'); break;
-       case '120': console.log('testy'); break;
-       case '102': console.log('testy'); break;
-       case '201': console.log('testy'); break;
-       case '210': console.log('testy'); break;
- // Middle 
-       case '345': console.log('testy'); break;
-       case '354': console.log('testy'); break;
-       case '543': console.log('testy'); break;
-       case '534': console.log('testy'); break;
-       case '453': console.log('testy'); break;
-       case '435': console.log('testy'); break;
- // bottom
-       case '678': console.log('testy'); break;
-       case '687': console.log('testy'); break;
-       case '786': console.log('testy'); break;
-       case '768': console.log('testy'); break;
-       case '876': console.log('testy'); break;
-       case '867': console.log('testy'); break; 
- // left
-       case '036': console.log('testy'); break;
-       case '063': console.log('testy'); break;
-       case '360': console.log('testy'); break;
-       case '306': console.log('testy'); break;
-       case '630': console.log('testy'); break;
-       case '603': console.log('testy'); break;
- // middle
-       case '147': console.log('testy'); break;
-       case '174': console.log('testy'); break;
-       case '417': console.log('testy'); break;
-       case '471': console.log('testy'); break;
-       case '741': console.log('testy'); break;
-       case '714': console.log('testy'); break;
- // right 
-       case '258': console.log('testy'); break;
-       case '285': console.log('testy'); break;
-       case '582': console.log('testy'); break;
-       case '528': console.log('testy'); break;
-       case '852': console.log('testy'); break;
-       case '825': console.log('testy'); break;
- // diagonal left
-       case '048': console.log('testy'); break;
-       case '084': console.log('testy'); break;
-       case '408': console.log('testy'); break;
-       case '480': console.log('testy'); break;
-       case '840': console.log('testy'); break;
-       case '804': console.log('testy'); break;
- // diagonal right
-       case '246': console.log('testy'); break;
-       case '264': console.log('testy'); break;
-       case '462': console.log('testy'); break;
-       case '426': console.log('testy'); break;
-       case '642': console.log('testy'); break;
-       case '624': console.log('testy'); break;
- 
-     }
+endButton.addEventListener("click",restart);
+
+function restart(e) {
+  headerDiv.className="header-display-show";
+  endDiv.className="end end-display-none";
+  whosGo = 0;
+  keepTrackPlayerOne = [];
+  keepTrackPlayerTwo = [];
+  for (prop of mainDiv.children) {
+    prop.innerHTML = '';
+    prop.addEventListener('click',addSVGs);
+
+  }
+  
+}
